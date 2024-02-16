63cc9e3dbf1247aca5fd8696eb2f7a88d560d74e
cole <garcia.ccole@gmail.com>
2024-02-16 12:00:43 +0800

    test

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post("/submit-inquiry", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
